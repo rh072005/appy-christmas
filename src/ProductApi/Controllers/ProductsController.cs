736588e175b153b8262978b490fd43f6d632464e
Richard Cooper <richard.cooper@bennetts.co.uk>
2016-11-15 22:16:13 +0000

    Add two more products

--- src/ProductApi/Controllers/ProductsController.cs
+++ src/ProductApi/Controllers/ProductsController.cs
@@ -13,25 +13,48 @@ namespace ProductApi.Controllers
         [HttpGet]
         public IEnumerable<Product> Get()
         {
-            return new Product[] {
-             new Product(),
-             new Product(),
-             new Product()   
-            };
+            return getProducts();
         }
 
         [HttpGet("{id}")]
         public Product Get(int id)
         {
-            return new Product {
-              Name = "Unicorn",
-              Type = "Toy",
-              Description = "The Unicorn with the Horn",
-              Manufacturer = "Deadpool Toys Ltd.",
-              Model = "Pinkie",
-              NetPrice = 0.12,
-              GrossPrice = 39.99 ,
-              ImageUrl = "images/unicorn.jpg"
+            return getProducts().ToArray()[id];
+        }
+
+        private IEnumerable<Product> getProducts()
+        {
+            return new Product[] { 
+                new Product {
+                    Name = "Unicorn",
+                    Type = "Toy",
+                    Description = "The Unicorn with the Horn",
+                    Manufacturer = "Deadpool Toys Ltd.",
+                    Model = "Pinkie",
+                    NetPrice = 0.12,
+                    GrossPrice = 39.99 ,
+                    ImageUrl = "images/unicorn.jpg"
+                },
+                new Product {
+                    Name = "Soup",
+                    Type = "Food",
+                    Description = "String Soup",
+                    Manufacturer = "Soup Dragon Foods.",
+                    Model = "",
+                    NetPrice = 1.00,
+                    GrossPrice = 1.20 ,
+                    ImageUrl = "images/soup.jpg"
+                },
+                new Product {
+                    Name = "Light Sabre",
+                    Type = "Food",
+                    Description = "Laser Sword",
+                    Manufacturer = "Jedi Manufacturing Ltd.",
+                    Model = "Blue",
+                    NetPrice = 100.00,
+                    GrossPrice = 250.00 ,
+                    ImageUrl = "images/lightsabre.jpg"
+                }
             };
         }
     }
